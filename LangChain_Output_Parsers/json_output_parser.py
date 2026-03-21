59df9e65a600db439efb2d9cb0769044653ad525
Bhavisha1803 <bhavisha.bhatia18@gmail.com>
2026-03-21 17:32:13 +0530

    output parsers

---
+++ LangChain_Output_Parsers/json_output_parser.py
@@ -0,0 +1,27 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+
+load_dotenv()
+
+model = ChatGroq(
+    model="llama-3.3-70b-versatile"
+    )
+
+parser = JsonOutputParser()
+
+template = PromptTemplate(
+    template='Give me 5 facts about {topic} \n {format_instruction}',
+    input_variables=['topic'],
+    partial_variables={'format_instruction': parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+result = chain.invoke({'topic':'black hole'})
+
+print(result)
+
+#we cant enforce a structure on the output using JsonOutputParser
+#for structured output we can use pydantic models and PydanticOutputParser or StructuredOutputParser
